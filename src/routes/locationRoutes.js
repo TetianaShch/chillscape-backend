b76c9fbdb3accc611276c3e6f44ae410e1a46510
Your Name <alisapodgaynaya023@gmail.com>
2026-04-06 01:32:36 -0600

    fix add and edit location

--- src/routes/locationRoutes.js
+++ src/routes/locationRoutes.js
@@ -1,25 +1,39 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import { authenticate } from '../middlewares/authenticate.js';
+import { uploadLocationImage } from '../middlewares/upload.js';
 import {
   getAllLocations,
   createLocation,
   updateLocation,
-  getLocationById
-  } from '../controllers/locationController.js';
+  getLocationById,
+} from '../controllers/locationController.js';
 import {
   getAllLocationsSchema,
   createLocationSchema,
   getLocationIdSchema,
-  updateLocationSchema
+  updateLocationSchema,
 } from '../validations/locationValidation.js';
 
 const router = Router();
 
 router.get('/', celebrate(getAllLocationsSchema), getAllLocations);
 router.get('/:locationId', celebrate(getLocationIdSchema), getLocationById);
-router.post('/', authenticate, celebrate(createLocationSchema), createLocation);
-router.patch('/:locationId', authenticate, celebrate(updateLocationSchema), updateLocation);
+
+router.post(
+  '/',
+  authenticate,
+  uploadLocationImage,
+  celebrate(createLocationSchema),
+  createLocation,
+);
+
+router.patch(
+  '/:locationId',
+  authenticate,
+  uploadLocationImage,
+  celebrate(updateLocationSchema),
+  updateLocation,
+);
 
 export default router;
-
--- src/validations/locationValidation.js
+++ src/validations/locationValidation.js
@@ -12,11 +12,13 @@ export const getAllLocationsSchema = {
       .optional(),
   }),
 };
+
 export const getLocationIdSchema = {
   [Segments.PARAMS]: Joi.object().keys({
     locationId: Joi.string().hex().length(24).required(),
   }),
 };
+
 export const createLocationSchema = {
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().min(3).max(96).required(),
@@ -24,7 +26,6 @@ export const createLocationSchema = {
     locationType: Joi.string().max(64).required(),
     region: Joi.string().max(64).required(),
     description: Joi.string().min(20).max(6000).required(),
-    images: Joi.array().items(Joi.string()).min(1).required(),
   }),
 };
 
@@ -38,6 +39,5 @@ export const updateLocationSchema = {
     locationType: Joi.string().max(64),
     region: Joi.string().max(64),
     description: Joi.string().min(20).max(6000),
-    images: Joi.array().items(Joi.string()).min(1),
   }),
 };
